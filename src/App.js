c09e4046da2c1f912784bc80a5e5f7e1d4ab9b12
Jonathan Sobotka <jsobotka@outlook.com>
2022-03-10 11:07:09 -0500

    Added create appointment feature

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Navbar from './components/navbar';
-import Home from './components/home';
-import Login from './components/login';
-import Signup from './components/signup';
-import Profile from './components/profile';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/navbar";
+import Home from "./components/home";
+import Login from "./components/login";
+import Signup from "./components/signup";
+import Profile from "./components/profile";
+import Appointment from "./components/appointment";
 function App() {
-  
-  
   return (
-      <BrowserRouter>
-        <div className="App">
-          <Navbar/>
-            <div>
-              <Routes>
-                <Route path="/" element={<Home/>} />
-                <Route path="/login" element={<Login/>} />
-                <Route path="/signup" element={<Signup/>} />
-                <Route path="/profile" element={<Profile/>} />
-              </Routes>
-            </div>
-            
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/appointment" element={<Appointment />} />
+          </Routes>
         </div>
-      </BrowserRouter>
+      </div>
+    </BrowserRouter>
   );
 }
 
