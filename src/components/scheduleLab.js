102ef04cca71390c39411cad264a8976a339aaa9
Jonathan Sobotka <jsobotka@outlook.com>
2022-03-31 13:11:30 -0400

    Added alert on lab creation

--- src/components/scheduleLab.js
+++ src/components/scheduleLab.js
@@ -1,22 +1,35 @@
+import { useState } from "react";
 const ScheduleLab = () => {
+  const [message, setMessage] = useState();
   const submit = (e) => {
-      e.preventDefault();
-      const testName = e.target.test.value;
-      const date = e.target.date.value;
-      const patientEmail = e.target.patientEmail.value;
-      const req = {testName, patientEmail, date};
-      fetch("http://localhost:8080/api/lab/create", {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(req),
-          credentials: "include",
-          
-      }).then(()=> {
-          console.log("created");
-
-      })
-  }
-
+    e.preventDefault();
+    const testName = e.target.test.value;
+    const date = e.target.date.value;
+    const patientEmail = e.target.patientEmail.value;
+    const req = { testName, patientEmail, date };
+    fetch("http://localhost:8080/api/lab/create", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(req),
+      credentials: "include",
+    }).then((response) => {
+      console.log(response.status);
+      if (response.status != 200) {
+        setMessage(
+          <h1 className="bg-danger" style={{ fontSize: "16px" }}>
+            Invalid Request, Please try again.
+          </h1>
+        );
+      } else {
+        setMessage(
+          <h1 className="bg-success" style={{ fontSize: "16px" }}>
+            Successfully added Lab for {e.target.patientEmail.value}
+          </h1>
+        );
+      }
+      console.log("created");
+    });
+  };
 
   return (
     <form onSubmit={submit}>
@@ -28,15 +41,23 @@ const ScheduleLab = () => {
               style={{ borderRadius: "1rem" }}
             >
               <div className="card-body p-5 text-center">
+                {message}
                 <div className="mb-md-5 mt-md-4 pb-5 row g-3">
                   <h2 className="font-weight-bold text-uppercase">Lab Form</h2>
 
-                  <p className="text-white-50 mb-5">Please fill out the following fields</p>
+                  <p className="text-white-50 mb-5">
+                    Please fill out the following fields
+                  </p>
                   <div className="col-md-6 form-outline form-white mb-4">
-                    <input type="text" name="patientEmail" className="form-control" required/>
+                    <input
+                      type="text"
+                      name="patientEmail"
+                      className="form-control"
+                      required
+                    />
                     <label className="form-label">Patient Email</label>
                   </div>
-    
+
                   <div className="col-md-6 form-outline form-white mb-4">
                     {/* <input
                       type="text"
